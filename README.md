efdaedc19427d3cdc2e42e4be66daffe9a048652
Leo Capuano <60476114+leo-capvano@users.noreply.github.com>
2024-03-13 09:40:32 +0100

    the first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Youtube Loader [Transcript]
+Welcome to Youtube Loader [Transcript], a Python package for extracting transcriptions from YouTube videos! This tool allows you to quickly and easily retrieve transcriptions in different languages, making it convenient for various language processing tasks.
+
+## Features
+- Extract transcriptions from YouTube videos.
+- Support for multiple languages.
+- Simple and easy-to-use interface.
+
+## Set-up
+> pip install -r requirements.txt  
+> streamlit run gui.py
+
+## Screenshots
+![img.png](img.png)
---
+++ gui.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from langchain_community.document_loaders import YoutubeLoader
+
+
+def transcript_video(yt_video_url: str, add_video_info: bool, languages: list, translation: str):
+    loader = YoutubeLoader.from_youtube_url(
+        yt_video_url,
+        add_video_info=add_video_info,
+        language=languages,
+        translation=translation
+    )
+    return loader.load()
+
+
+st.title("Youtube Loader [Transcript]")
+st.text_area("Insert here a youtube url", key="yt_url_input")
+st.selectbox(label="Select translation language", key="translation_lang", options=("en", "it"), index=0)
+if st.button("Transcript"):
+    yt_url = st.session_state.yt_url_input
+    translation_lang = st.session_state.translation_lang
+    with st.spinner("Getting transcription ..."):
+        video_transcription = transcript_video(yt_url, True, ["en"], translation_lang)
+        with st.container(border=True):
+            st.json(video_transcription)
